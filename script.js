57b816508402e3d5beff2f355b0a4b830b4c95d3
fkingerd <spelrond21@gmail.com>
2024-01-07 23:39:23 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('poo-emoji').addEventListener('mouseover', function() {
+    this.style.transform = 'rotate(20deg)';
+});
+
+document.getElementById('poo-emoji').addEventListener('mouseout', function() {
+    this.style.transform = 'rotate(0deg)';
+});
